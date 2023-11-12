2d07daebe007cbdbd177034ecf1056a6b4a2b2c6
NeoQenXzin <keyntao99@gmail.com>
2023-11-12 17:51:58 +0100

    project beta version

---
+++ main.js
@@ -0,0 +1,131 @@
+import "./style.css";
+
+class Game {
+  constructor() {
+    this.userGuess = document.getElementById("your-guess");
+    this.guessResult = document.getElementById("guess-result");
+    this.myscore = document.getElementById("myscore");
+    this.schem = document.getElementById("your-try-schem");
+    this.restartButton = document.getElementById("restart");
+    this.attemptCount = 0;
+    this.score = 0;
+    this.guessMarkers = [];
+    this.targetNumber = null;
+  }
+
+  startGame() {
+    this.targetNumber = this.getRandomNumber(0, 500);
+    this.playGuessing(this.targetNumber);
+  }
+
+  // Generate a random number within a range
+  getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  // Validate the user's input
+  isValidNumber(number) {
+    return !Number.isNaN(number) && number >= 0 && number <= 500;
+  }
+
+  playGuessing(targetNumber) {
+    const userChoiceSubmit = document.getElementById("user-choice-submit");
+
+    userChoiceSubmit?.addEventListener("click", () => {
+      // @ts-ignore
+      let userGuessValue = this.userGuess.value;
+
+      // Valider la saisie de l'utilisateur
+      if (!this.isValidNumber(userGuessValue)) {
+        this.guessResult.innerText =
+          "🛑 The entered number is invalid. It must be between 0 and 500.\n\n";
+        return;
+      }
+      if (userGuessValue > targetNumber) {
+        this.guessResult.innerText = `🔴 The entered number is **too big** than ${userGuessValue}.\n\n`;
+        this.attemptCount += 1;
+        this.score = this.attemptCount * 5;
+        this.myscore.innerText = this.score;
+        this.schem.innerText = "🔸";
+        this.addMyGuessToShem(false);
+
+        // return playGuessing();
+      }
+
+      if (userGuessValue < targetNumber) {
+        this.guessResult.innerText = `🔴 The entered number is **too small** than ${userGuessValue}.\n\n`;
+        this.attemptCount += 1;
+        this.score = this.attemptCount * 5;
+        this.myscore.innerText = this.score;
+        this.addMyGuessToShem(false);
+        console.log(targetNumber);
+        // return playGuessing();
+      }
+      if (userGuessValue == targetNumber) {
+        this.attemptCount += 1;
+        this.score = this.attemptCount * 5;
+        this.myscore.innerText = this.score;
+        this.guessResult.innerText = `🟢 Well done! The random number was indeed ${userGuessValue}. 
+        ✨ You succeeded in ${this.attemptCount} attempts.`;
+        this.addMyGuessToShem(true);
+        this.restartButton.addEventListener("click", () => {
+          this.restartGame();
+        });
+        this.restartButton?.classList.remove("hidden");
+      }
+      userGuessValue = "";
+    });
+  }
+  addMyGuessToShem(boolean) {
+    const widthSchem = this.schem?.getBoundingClientRect().width;
+    // Position de la supposition sur le schéma en fonction de la largeur du schéma
+    const positionOnSchem = (this.userGuess.value / 500) * (widthSchem - 25);
+
+    // Créer un élément span pour représenter la supposition sur le schéma
+    const guessMarker = document.createElement("span");
+    guessMarker.style.position = "absolute";
+    guessMarker.style.bottom = "40%";
+    guessMarker.style.left = `${positionOnSchem}px`;
+
+    // Utiliser ✅ pour une réponse correcte et 🔸 pour une réponse incorrecte
+    guessMarker.innerText = boolean === true ? "✅" : "🔸";
+
+    // Ajouter l'élément au schéma
+    this.schem?.appendChild(guessMarker);
+    this.guessMarkers.push(guessMarker); // Ajouter le marqueur à la liste
+
+    this.renderGuesses();
+  }
+  renderGuesses() {
+    // Effacer le contenu actuel du schéma
+    this.schem.innerHTML = "";
+
+    // Ajouter tous les marqueurs de supposition au schéma
+    this.guessMarkers.forEach((guessMarker) => {
+      this.schem?.appendChild(guessMarker);
+    });
+  }
+
+  restartGame() {
+    console.log("fonctionne");
+    this.attemptCount = 0;
+    this.score = 0;
+    this.guessMarkers = [];
+    this.myscore.innerText = this.score;
+    this.guessResult.innerText = ``;
+    this.renderGuesses();
+    this.restartButton?.classList.add("hidden");
+    this.startGame();
+  }
+}
+
+// Démarrer une partie page load game
+const startButton = document.getElementById("button-start");
+const starterPage = document.getElementById("starter-page");
+const gamePage = document.getElementById("game-page");
+startButton?.addEventListener("click", () => {
+  starterPage?.classList.add("hidden");
+  gamePage?.classList.remove("hidden");
+  const game = new Game();
+  game.startGame();
+});
